90c43f81af20348dfe5492813bc46adb0f73d919
Eric Moran <emoran0403@gmail.com>
2022-03-16 16:04:44 -0400

    added todos for tomorrow

--- client/app.js
+++ client/app.js
@@ -4,29 +4,41 @@ const Chirpbox = $("#Chirpbox");
 const usernameBox = $("#username");
 
 addChirpButton.click((e) => {
-  e.preventDefault();
-  const message = Chirpbox.val();
-  const username = usernameBox.val();
-  if (!message || !username) {
-    swal.fire("fill in the boxes");
+  e.preventDefault(); // stops the button from refreshing the page
+  const message = Chirpbox.val(); // store the value of the text in the chirpbox
+  const username = usernameBox.val(); // store the value of the text in the username box
+  if (!message) {
+    // if there is nothing in the chirpbox, fire an alert
+    swal.fire("Your chirp needs some text!");
     return;
   }
+
+  if (!username) {
+    // if there is nothing in the username box, fire an alert
+    swal.fire("Don't forget your username!");
+    return;
+  }
+
   fetch("/api/chirps/", {
-    method: "POST",
+    // contact /api/chirps/ ...
+    method: "POST", // ...with a POST request...
     headers: {
+      // ...specifying the type of content...
       "content-type": "application/json",
     },
-    body: JSON.stringify({ message, username }),
+    body: JSON.stringify({ message, username }), // ...and deliver the content
   })
     .then((data) => data.json())
     .then((data) => {
       console.log(data);
-      Chirpbox.val("");
+      Chirpbox.val(""); // clears out the form boxes
       usernameBox.val("");
     })
     .catch((error) => console.log(error));
 });
 
+//! I need to get all chirps, then play with the data structure to turn it into an array and remove the next id key
+// make the button call the deleteChirp function with the chirp ID
 function getChirps() {
   fetch("/api/chirps/")
     .then((data) => data.json())
@@ -36,7 +48,12 @@ function getChirps() {
         data.map(
           (chirp) =>
             `<div class="card">
-            <div class="card-body">${chirp.message}</div>
+                <div class="card-body>
+                    <h5 class="card-title">${chirp.username}</h5>
+                    <p class="card-text">${chirp.message}</p>
+                    <button onClick(() => {${chirp.id}})>Unchirp</button>
+                    <button onClick(() => {${chirp.id}})>Rechirp</button> 
+                </div>
             </div>`
         )
       );
@@ -47,6 +64,7 @@ function getChirps() {
 getChirps();
 
 function deleteChirp(id) {
+  // contact /api/chirps/:id with a DELETE request to delete the specified chirp
   fetch(`/api/chirps/${id}`, { method: "DELETE" });
 }
 
